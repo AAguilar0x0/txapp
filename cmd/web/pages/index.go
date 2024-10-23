cf6184d3060f392eedca1931e815c9625c8a3fb1
Adrian Vincent Aguilar <aguilaradrianvincent@gmail.com>
2024-10-23 16:13:24 +0800

    feat(web/pages): add fallback page not found

--- cmd/web/pages/index.go
+++ cmd/web/pages/index.go
@@ -1,6 +1,7 @@
 package pages
 
 import (
+	"github.com/AAguilar0x0/bapp/cmd/web/pages/components"
 	"github.com/AAguilar0x0/bapp/cmd/web/pages/utils"
 	"github.com/AAguilar0x0/bapp/cmd/web/types"
 	"github.com/labstack/echo/v4"
@@ -11,8 +12,13 @@ type handler types.Handler
 func New(e *echo.Group, h *types.Handler) {
 	d := (*handler)(h)
 	e.GET("", d.get)
+	e.Any("*", d.fallback)
 }
 
 func (d *handler) get(c echo.Context) error {
 	return utils.Render(c, guest())
+}
+
+func (d *handler) fallback(c echo.Context) error {
+	return utils.Render(c, components.PageNotFound())
 }
