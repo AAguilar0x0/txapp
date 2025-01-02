be06fcc1c0ffc5f38e95f3f189ea5c4e081e5fd3
Adrian Vincent Aguilar <aguilaradrianvincent@gmail.com>
2025-01-02 10:13:41 +0800

    feat(apierrors): add stack trace and new HTTP status handlers
    - Add stack trace field to APIError struct
    - Add Inherit method for error chaining
    - Add new HTTP status code handlers

--- core/pkg/apierrors/index.go
+++ core/pkg/apierrors/index.go
@@ -14,6 +14,7 @@ type APIError struct {
 	Status     int
 	Message    string
 	ErrorCause string
+	stackTrace string
 }
 
 func (d *APIError) Error() string {
@@ -28,9 +29,17 @@ func (d *APIError) Error() string {
 		ecRaw, err := json.Marshal(d.ErrorCause)
 		assert.NoError(err, "marshalling ErrorCause", "fault", "Marshal")
 		format += " error=%s\nstack: %s"
-		datas = append(datas, string(ecRaw), string(debug.Stack()))
+		datas = append(datas, string(ecRaw), d.stackTrace)
 	}
 	return fmt.Sprintf(format, datas...)
+}
+
+func (d *APIError) Inherit(err *APIError) *APIError {
+	if err != nil {
+		d.ErrorCause = err.ErrorCause
+		d.stackTrace = err.stackTrace
+	}
+	return d
 }
 
 func New(status int, message string, args []string) *APIError {
@@ -41,8 +50,29 @@ func New(status int, message string, args []string) *APIError {
 	length := len(args)
 	if length > 0 {
 		err.ErrorCause = args[0]
+		err.stackTrace = string(debug.Stack())
 	}
 	return &err
+}
+
+func Processing(message string, args ...string) *APIError {
+	return New(http.StatusProcessing, message, args)
+}
+
+func Ok(message string, args ...string) *APIError {
+	return New(http.StatusOK, message, args)
+}
+
+func Accepted(message string, args ...string) *APIError {
+	return New(http.StatusAccepted, message, args)
+}
+
+func NoContent(message string, args ...string) *APIError {
+	return New(http.StatusNoContent, message, args)
+}
+
+func TemporaryRedirect(message string, args ...string) *APIError {
+	return New(http.StatusTemporaryRedirect, message, args)
 }
 
 func BadRequest(message string, args ...string) *APIError {
@@ -69,8 +99,32 @@ func RequestTimeout(message string, args ...string) *APIError {
 	return New(http.StatusRequestTimeout, message, args)
 }
 
+func PreconditionFailed(message string, args ...string) *APIError {
+	return New(http.StatusPreconditionFailed, message, args)
+}
+
+func RequestEntityTooLarge(message string, args ...string) *APIError {
+	return New(http.StatusRequestEntityTooLarge, message, args)
+}
+
+func UnsupportedMediaType(message string, args ...string) *APIError {
+	return New(http.StatusUnsupportedMediaType, message, args)
+}
+
+func UnprocessableEntity(message string, args ...string) *APIError {
+	return New(http.StatusUnprocessableEntity, message, args)
+}
+
+func TooManyRequest(message string, args ...string) *APIError {
+	return New(http.StatusTooManyRequests, message, args)
+}
+
 func InternalServerError(message string, args ...string) *APIError {
 	return New(http.StatusInternalServerError, message, args)
+}
+
+func NotImplemented(message string, args ...string) *APIError {
+	return New(http.StatusNotImplemented, message, args)
 }
 
 func ServiceUnavailable(message string, args ...string) *APIError {
